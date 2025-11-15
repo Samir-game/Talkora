69efcc06145a7e108837f140664ef0f2d59aee49
Samir-game <samirgame01@gmail.com>
2025-11-15 20:36:13 +0530

    completed user model, auth, stream setup

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import {connectDB} from "./lib/db.js"
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -10,6 +11,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
