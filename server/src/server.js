027da5aea03e6ab52f8523c37ef5071b61732304
Samir-game <samirgame01@gmail.com>
2025-11-18 23:57:22 +0530

    some changes

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ import {connectDB} from "./lib/db.js"
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -11,6 +12,9 @@ import chatRoutes from "./routes/chat.route.js";
 
 const app = express();
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
+
 
 app.use(cors({
   origin:"http://localhost:5173",
@@ -24,6 +28,16 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/chat",chatRoutes);
 
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  // Catch-all route for SPA
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+
+}
 
 
 app.listen(PORT, () => {
